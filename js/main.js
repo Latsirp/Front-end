b99dee5a6d9df7e08cde882db1b3c209f0085042
Latsirp <enzo_scholl@estudante.sesisenai.org.br>
2024-09-04 09:43:06 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const ativarMenuLateral = document.getElementById("ativarMenuLateral"); // Pegando o ID de ativarMenuLateral no HTML
+const menuLateral = document.getElementById("menuLateral"); // Pegando o ID de menuLateral no HTML
+
+// Criando um evento que ativará e desativará o menu lateral da página
+ativarMenuLateral.addEventListener("click", function () {});
