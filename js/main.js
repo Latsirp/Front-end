8074132f0571afdc3ac61f5c0f5834eb8a638281
Latsirp <enzo_scholl@estudante.sesisenai.org.br>
2024-09-05 21:26:52 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,5 +0,0 @@
-const ativarMenuLateral = document.getElementById("ativarMenuLateral"); // Pegando o ID de ativarMenuLateral no HTML
-const menuLateral = document.getElementById("menuLateral"); // Pegando o ID de menuLateral no HTML
-
-// Criando um evento que ativará e desativará o menu lateral da página
-ativarMenuLateral.addEventListener("click", function () {});
