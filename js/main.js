3f2d405289355459f4df12f0332588e559a0c257
Latsirp <enzo_scholl@estudante.sesisenai.org.br>
2024-09-06 21:07:51 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 // Criando um evento que utiliza-se do ID do botão para abrir e fechar o menu lateral
 document.getElementById("abrir-menu").addEventListener("click", () => {
-  const menuLateral = document.querySelector("header");
+  const menuLateral = document.getElementById("menu-lateral");
+  const conteudo = document.querySelector("main", "footer");
+  const footer = document.querySelector("footer");
   if (menuLateral.classList.contains("-translate-x-full")) {
     menuLateral.classList.remove("-translate-x-full");
     menuLateral.classList.add("translate-x-0");
+    conteudo.classList.add("brightness-50");
+    footer.classList.add("brightness-50");
   } else {
     menuLateral.classList.remove("translate-x-0");
+    conteudo.classList.remove("brightness-50");
+    footer.classList.remove("brightness-50");
     menuLateral.classList.add("-translate-x-full");
   }
 });
